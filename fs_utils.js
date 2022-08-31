bfe681553b2cfa847b286d5f6dc53298edaac8db
Agustin <agustin.rufino@silverfin.com>
2022-08-31 17:18:43 +0200

    Liquid Test YAML fetched from the API Now the test can be fetched if it is stored in Silverfin We won't overwrite existin YAML Also, createFiles function has been splitted into two. One for Liquid Testing and one for Templates)

--- fs_utils.js
+++ fs_utils.js
@@ -4,6 +4,7 @@ const path = require('path');
 function createFolder(path) {
   if (!fs.existsSync(path)) {
     fs.mkdirSync(path);
+    console.log(`Directory created: ${path}`);
   };
 };
 
@@ -13,48 +14,59 @@ function createFolders(relativePath) {
   createFolder(`${relativePath}/text_parts`);
 };
 
-async function createFiles({ relativePath, testFile, textParts, text }) {
+async function createLiquidTestFiles(relativePath, testFilenameRoot, testContent) {
   const emptyCallback = () => {};
-
-  if (!fs.existsSync(`${relativePath}/tests/${testFile.name}.yml`)) {
-    fs.writeFile(`${relativePath}/tests/${testFile.name}.yml`, testFile.content, emptyCallback);
+  // Liquid Test: YAML
+  if (!fs.existsSync(`${relativePath}/tests/${testFilenameRoot}_liquid_test.yml`)) {
+    fs.writeFile(`${relativePath}/tests/${testFilenameRoot}_liquid_test.yml`, testContent, emptyCallback);
+    console.log(`Liquid Testing: YAML file created`);
   };
-
+  // Liquid Test: Readme
   if (!fs.existsSync(`${relativePath}/tests/README.md`)) {
     const readmeLiquidTests = fs.readFileSync(path.resolve(__dirname,'./resources/liquid_tests/README.md'), 'UTF-8');
     fs.writeFileSync(`${relativePath}/tests/README.md`, readmeLiquidTests);
+    console.log(`Liquid Testing: README file created`);
   };
+};
 
+async function createTemplateFiles(relativePath, textMain, textParts) {
+  const emptyCallback = () => {};
+  // Template: Main
+  fs.writeFile(`${relativePath}/main.liquid`, textMain, emptyCallback);
+  console.log(`Template: main part file created`);  
+  // Template: Parts
   Object.keys(textParts).forEach((textPartName) => {
     if (textPartName) {
       fs.writeFile(`${relativePath}/text_parts/${textPartName}.liquid`, textParts[textPartName], emptyCallback);
+      console.log(`Template: ${textPartName} file created`);
     };
   });
-
-  fs.writeFile(`${relativePath}/main.liquid`, text, emptyCallback);
 };
 
 async function createLiquidFile(relativePath, fileName, textContent) {
   const emptyCallback = () => {};
   fs.writeFile(`${relativePath}/${fileName}.liquid`, textContent, emptyCallback);
+  console.log(`${fileName} file created`);
 };
 
 function writeConfig(relativePath, config) {
   emptyCallback = () => {};
   fs.writeFile(`${relativePath}/config.json`, JSON.stringify(config, null, 2), emptyCallback);
+  console.log(`Template: config.json file created`);
 };
 
 function readConfig(relativePath) {
   const json = fs.readFileSync(`${relativePath}/config.json`).toString();
   const config = JSON.parse(json);
-
   return config;
 };
 
 module.exports = { 
   writeConfig,
-  createFiles, 
+  createTemplateFiles,
+  createLiquidTestFiles,
   createLiquidFile, 
   createFolder, 
   createFolders, 
   readConfig };
+  
