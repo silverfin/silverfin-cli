20679837660ca87c6f43d3e59534bbcacc966e1a
Wouter Bruynsteen <wouter.bruynsteen@silverfin.com>
2022-10-12 13:21:37 +0200

    Format JS files

--- fs_utils.js
+++ fs_utils.js
@@ -1,34 +1,47 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 function createFolder(path) {
   if (!fs.existsSync(path)) {
     fs.mkdirSync(path);
-  };
-};
+  }
+}
 
 function createFolders(relativePath) {
   createFolder(relativePath);
   createFolder(`${relativePath}/tests`);
   createFolder(`${relativePath}/text_parts`);
-};
+}
 
-async function createLiquidTestFiles(relativePath, testFilenameRoot, testContent) {
+async function createLiquidTestFiles(
+  relativePath,
+  testFilenameRoot,
+  testContent
+) {
   const emptyCallback = () => {};
   // Liquid Test: YAML
-  if (!fs.existsSync(`${relativePath}/tests/${testFilenameRoot}_liquid_test.yml`)) {
-    fs.writeFile(`${relativePath}/tests/${testFilenameRoot}_liquid_test.yml`, testContent, emptyCallback);
-  };
+  if (
+    !fs.existsSync(`${relativePath}/tests/${testFilenameRoot}_liquid_test.yml`)
+  ) {
+    fs.writeFile(
+      `${relativePath}/tests/${testFilenameRoot}_liquid_test.yml`,
+      testContent,
+      emptyCallback
+    );
+  }
   // Liquid Test: Readme
   if (!fs.existsSync(`${relativePath}/tests/README.md`)) {
-    const readmeLiquidTests = fs.readFileSync(path.resolve(__dirname,'./resources/liquid_tests/README.md'), 'UTF-8');
+    const readmeLiquidTests = fs.readFileSync(
+      path.resolve(__dirname, "./resources/liquid_tests/README.md"),
+      "UTF-8"
+    );
     fs.writeFileSync(`${relativePath}/tests/README.md`, readmeLiquidTests);
-  };
-  
+  }
+
   if (relativePath) {
-    console.log(`Liquid testing file(s) created for ${relativePath}`)
-  }  
-};
+    console.log(`Liquid testing file(s) created for ${relativePath}`);
+  }
+}
 
 async function createTemplateFiles(relativePath, textMain, textParts) {
   const emptyCallback = () => {};
@@ -37,38 +50,50 @@ async function createTemplateFiles(relativePath, textMain, textParts) {
   // Template: Parts
   Object.keys(textParts).forEach((textPartName) => {
     if (textPartName) {
-      fs.writeFile(`${relativePath}/text_parts/${textPartName}.liquid`, textParts[textPartName], emptyCallback);
-    };
+      fs.writeFile(
+        `${relativePath}/text_parts/${textPartName}.liquid`,
+        textParts[textPartName],
+        emptyCallback
+      );
+    }
   });
 
   if (relativePath) {
-    console.log(`Liquid template file(s) created for ${relativePath}`)
-  }  
-};
+    console.log(`Liquid template file(s) created for ${relativePath}`);
+  }
+}
 
 async function createLiquidFile(relativePath, fileName, textContent) {
   const emptyCallback = () => {};
-  fs.writeFile(`${relativePath}/${fileName}.liquid`, textContent, emptyCallback);
+  fs.writeFile(
+    `${relativePath}/${fileName}.liquid`,
+    textContent,
+    emptyCallback
+  );
   console.log(`${fileName} file created`);
-};
+}
 
 function writeConfig(relativePath, config) {
   emptyCallback = () => {};
-  fs.writeFile(`${relativePath}/config.json`, JSON.stringify(config, null, 2), emptyCallback);
-};
+  fs.writeFile(
+    `${relativePath}/config.json`,
+    JSON.stringify(config, null, 2),
+    emptyCallback
+  );
+}
 
 function readConfig(relativePath) {
   const json = fs.readFileSync(`${relativePath}/config.json`).toString();
   const config = JSON.parse(json);
   return config;
-};
+}
 
-module.exports = { 
+module.exports = {
   writeConfig,
   createTemplateFiles,
   createLiquidTestFiles,
-  createLiquidFile, 
-  createFolder, 
-  createFolders, 
-  readConfig };
-  
+  createLiquidFile,
+  createFolder,
+  createFolders,
+  readConfig,
+};
--- test.js
+++ test.js
@@ -1,8 +1,8 @@
-const toolkit = require('./index.js');
-const fs = require('fs');
+const toolkit = require("./index.js");
+const fs = require("fs");
 
 test.only("createNewTemplateFolder", async () => {
-  const handlerName = "super_handler"
+  const handlerName = "super_handler";
   expect(fs.existsSync(handlerName)).toBe(false);
 
   await toolkit.createNewTemplateFolder(handlerName);
@@ -15,22 +15,26 @@ test.only("createNewTemplateFolder", async () => {
 });
 
 test("importNewTemplateFolder", async () => {
-  const handlerName = "expenses"
+  const handlerName = "expenses";
   expect(fs.existsSync(handlerName)).toBe(false);
 
   await toolkit.importNewTemplateFolder(handlerName);
 
   expect(fs.existsSync(handlerName)).toBe(true);
   expect(fs.existsSync(`./${handlerName}/tests/test.yml`)).toBe(true);
-  expect(fs.existsSync(`./${handlerName}/text_parts/variables.liquid`)).toBe(true);
-  expect(fs.existsSync(`./${handlerName}/text_parts/translations.liquid`)).toBe(true);
+  expect(fs.existsSync(`./${handlerName}/text_parts/variables.liquid`)).toBe(
+    true
+  );
+  expect(fs.existsSync(`./${handlerName}/text_parts/translations.liquid`)).toBe(
+    true
+  );
   expect(fs.existsSync(`./${handlerName}/config.json`)).toBe(true);
 
   fs.rmdirSync(handlerName, { recursive: true });
 });
 
 test.only("constructReconciliationText", async () => {
-  const handlerName = "expenses"
+  const handlerName = "expenses";
 
   toolkit.constructReconciliationText(handlerName);
-})
+});
