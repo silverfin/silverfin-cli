651ae1fec0c76f4f378b0b4b3bd000043edeacd5
Wouter Bruynsteen <99254741+Woetfin@users.noreply.github.com>
2023-09-14 23:01:15 +0200

    Support HTML render for input and/or preview mode from new /reconciliations/render endpoint (#55)
    * update run-test CLI command options + replace 'html_render' with new 'mode' property
    
    * rename checkMode to checkRenderMode
    
    * add createPreviewRun function + clean-up spinner on forced exit and 'ENTER' keypress during running process
    
    * update spinner code formatting
    
    * return testRun & previewRun
    
    * only support CTRL+c to force spinner stop
    
    * handle rendered HTMLs for the browser + handle previewOnly case
    
    * update package.json
    
    * improve clearLine for spinner
    
    * fix liquid test HTML rendering in development-mode
    
    * Check preview-only flag is not used alone
    
    ---------
    
    Co-authored-by: Agustin <agustin.rufino@silverfin.com>

--- bin/cli.js
+++ bin/cli.js
@@ -278,8 +278,18 @@ program
     ""
   )
   .option(
-    "--html",
-    "Get a html file of the template generated with the Liquid Test information (optional)",
+    "--html-input",
+    "Get a static html of the input-view of the template generated with the Liquid Test data (optional)",
+    false
+  )
+  .option(
+    "--html-preview",
+    "Get a static html of the export-view of the template generated with the Liquid Test data (optional)",
+    false
+  )
+  .option(
+    "--preview-only",
+    "Skip the checking of the results of the Liquid Test in case you only want to generate a preview template (optional)",
     false
   )
   .option(
@@ -296,11 +306,19 @@ program
         options.test
       );
     } else {
+      if (options.previewOnly && !options.htmlInput && !options.htmlPreview) {
+        console.log(
+          `When using "--preview-only" you need to specify at least one of the following options: "--html-input", "--html-preview"`
+        );
+        process.exit(1);
+      }
       liquidTestRunner.runTestsWithOutput(
         options.firm,
         options.handle,
         options.test,
-        options.html
+        options.previewOnly,
+        options.htmlInput,
+        options.htmlPreview
       );
     }
   });
@@ -455,7 +473,7 @@ program
   )
   .option(
     "--html",
-    `Get a html file of the template generated with the Liquid Test information (optional). It has to be used together with "--handle"`,
+    `Get a html file of the template's input-view generated with the Liquid Test information (optional). It has to be used together with "--handle"`,
     false
   )
   .option("--yes", "Skip the prompt confirmation (optional)")
