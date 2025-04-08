6569561b870b0eb17536e7298a26403092274425
delphine-demeulenaere <delphine.demeulenaere@silverfin.com>
2025-04-08 16:47:03 +0200

    add more tests for AT's (WIP)

--- tests/lib/templates/accountTemplates.test.js
+++ tests/lib/templates/accountTemplates.test.js
@@ -13,6 +13,8 @@ describe("AccountTemplate", () => {
     const textParts = { part_1: "Part 1: updated content" };
     const template = {
       name_nl: "name_nl",
+      name_en: "name_nl",
+      name_fr: "name_nl",
       id: 808080,
       text: "Main liquid content",
       text_parts: [
