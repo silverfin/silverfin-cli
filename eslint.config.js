c22715169a79dd04ab379af09af2da057662d42f
Agustin <agustin.rufino@silverfin.com>
2025-05-26 11:02:56 +0200

    Create ESLint configuration
    Create a basic eslint configuration using the js-recommended rules. The
    goal would be to introduce a github action that runs the linter in Pull
    Requests in the future. First, but first we should address current
    errors being raised

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+module.exports = [
+  require("@eslint/js").configs.recommended,
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "commonjs",
+      ecmaVersion: "latest",
+      globals: {
+        ...require("globals").node,
+        ...require("globals").jest,
+      },
+    },
+    rules: {
+      "no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+        },
+      ],
+      "prefer-const": "error",
+    },
+  },
+];
