7ec1918d6cc1c263b59aa9159e050443bd55d81d
Michiel Degezelle <michiel.degezelle@silverfin.com>
2025-06-03 13:44:37 +0200

    Fix linter warning

--- tests/lib/templates/exportFiles.test.js
+++ tests/lib/templates/exportFiles.test.js
@@ -1,7 +1,6 @@
 const fs = require("fs");
 const fsPromises = require("fs").promises;
 const path = require("path");
-const fsUtils = require("../../../lib/utils/fsUtils");
 const templateUtils = require("../../../lib/utils/templateUtils");
 const { ExportFile } = require("../../../lib/templates/exportFile");
 
