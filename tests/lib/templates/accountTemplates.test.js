246ca4a0245c948606db4b5ad762c78ed5a28ccd
Michiel Degezelle <michiel.degezelle@silverfin.com>
2025-06-03 13:46:42 +0200

    Fix linter warnings

--- tests/lib/templates/accountTemplates.test.js
+++ tests/lib/templates/accountTemplates.test.js
@@ -1,7 +1,6 @@
 const fs = require("fs");
 const fsPromises = require("fs").promises;
 const path = require("path");
-const fsUtils = require("../../../lib/utils/fsUtils");
 const templateUtils = require("../../../lib/utils/templateUtils");
 const { AccountTemplate } = require("../../../lib/templates/accountTemplate");
 
@@ -40,7 +39,6 @@ describe("AccountTemplate", () => {
       text_parts: {
         part_1: "text_parts/part_1.liquid",
       },
-      name_nl: "name_nl",
       name_fr: "name_nl",
       name_en: "name_nl",
       account_range: null,
@@ -56,12 +54,11 @@ describe("AccountTemplate", () => {
         old_part: "text_parts/old_part.liquid",
         part_1: "text_parts/part_1.liquid",
       },
-      name_nl: "name_nl",
-	  name_fr: "name_fr",
-	  name_en: "name_en",
-	  account_range: null,
-	  mapping_list_ranges: [],
-	  hide_code: false,
+      name_fr: "name_fr",
+      name_en: "name_en",
+      account_range: null,
+      mapping_list_ranges: [],
+      hide_code: false,
       published: true,
     };
 
@@ -87,139 +84,136 @@ describe("AccountTemplate", () => {
       jest.resetAllMocks();
     });
 
-	it("should return false if name_nl is missing", () => {
-		const result = AccountTemplate.save("firm", 100, { id: 808080 });
-		expect(result).toBe(false);
-	});
+    it("should return false if name_nl is missing", () => {
+      const result = AccountTemplate.save("firm", 100, { id: 808080 });
+      expect(result).toBe(false);
+    });
 
-	it("should return false if the liquid code is missing", () => {
-		templateUtils.missingLiquidCode.mockReturnValue(true);
-		const result = AccountTemplate.save("firm", 100, template);
-		expect(result).toBe(false);
-		expect(templateUtils.missingLiquidCode).toHaveBeenCalledWith(template);
-	  });
+    it("should return false if the liquid code is missing", () => {
+      templateUtils.missingLiquidCode.mockReturnValue(true);
+      const result = AccountTemplate.save("firm", 100, template);
+      expect(result).toBe(false);
+      expect(templateUtils.missingLiquidCode).toHaveBeenCalledWith(template);
+    });
 
-	it("should return false if the template handle is invalid", () => {
-		templateUtils.checkValidName.mockReturnValue(false);
-		const result = AccountTemplate.save("firm", 100, template);
-		expect(result).toBe(false);
-		expect(templateUtils.checkValidName).toHaveBeenCalledWith("name_nl", "accountTemplate");
-	});
+    it("should return false if the template handle is invalid", () => {
+      templateUtils.checkValidName.mockReturnValue(false);
+      const result = AccountTemplate.save("firm", 100, template);
+      expect(result).toBe(false);
+      expect(templateUtils.checkValidName).toHaveBeenCalledWith("name_nl", "accountTemplate");
+    });
 
-  it("should create the necessary files and store template's relevant details", async () => {
-    templateUtils.missingLiquidCode.mockReturnValue(false);
-    templateUtils.checkValidName.mockReturnValue(true);
-    templateUtils.filterParts.mockReturnValue({ part_1: "Part 1 content" });
+    it("should create the necessary files and store template's relevant details", async () => {
+      templateUtils.missingLiquidCode.mockReturnValue(false);
+      templateUtils.checkValidName.mockReturnValue(true);
+      templateUtils.filterParts.mockReturnValue({ part_1: "Part 1 content" });
 
-    await AccountTemplate.save("firm", 100, template);
+      await AccountTemplate.save("firm", 100, template);
 
-    // Check folder creation
-    expect(fs.existsSync(expectedFolderPath)).toBe(true);
-    // Check main liquid file
-    expect(fs.existsSync(mainLiquidPath)).toBe(true);
-    const mainLiquidContent = await fsPromises.readFile(mainLiquidPath, "utf-8");
-    expect(mainLiquidContent).toBe(template.text);
-    // Check text parts liquid files
-    expect(fs.existsSync(part1LiquidPath)).toBe(true);
-    const part1LiquidContent = await fsPromises.readFile(part1LiquidPath, "utf-8");
-    expect(part1LiquidContent).toBe("Part 1 content");
-    // Check config file
-    expect(fs.existsSync(configPath)).toBe(true);
-    const configSaved = JSON.parse(await fsPromises.readFile(configPath, "utf-8"));
-    expect(configSaved).toEqual(configToWrite);    
-    // Check liquid test file
-    expect(fs.existsSync(testLiquidPath)).toBe(true);
-    const testLiquidContent = await fsPromises.readFile(testLiquidPath, "utf-8");
-    expect(testLiquidContent).toBe(template.tests);
+      // Check folder creation
+      expect(fs.existsSync(expectedFolderPath)).toBe(true);
+      // Check main liquid file
+      expect(fs.existsSync(mainLiquidPath)).toBe(true);
+      const mainLiquidContent = await fsPromises.readFile(mainLiquidPath, "utf-8");
+      expect(mainLiquidContent).toBe(template.text);
+      // Check text parts liquid files
+      expect(fs.existsSync(part1LiquidPath)).toBe(true);
+      const part1LiquidContent = await fsPromises.readFile(part1LiquidPath, "utf-8");
+      expect(part1LiquidContent).toBe("Part 1 content");
+      // Check config file
+      expect(fs.existsSync(configPath)).toBe(true);
+      const configSaved = JSON.parse(await fsPromises.readFile(configPath, "utf-8"));
+      expect(configSaved).toEqual(configToWrite);
+      // Check liquid test file
+      expect(fs.existsSync(testLiquidPath)).toBe(true);
+      const testLiquidContent = await fsPromises.readFile(testLiquidPath, "utf-8");
+      expect(testLiquidContent).toBe(template.tests);
+    });
+
+    it("should fetch an existing template's config and update with new details", async () => {
+      templateUtils.missingLiquidCode.mockReturnValue(false);
+      templateUtils.checkValidName.mockReturnValue(true);
+      templateUtils.filterParts.mockReturnValue(textParts);
+
+      fs.mkdirSync(path.join(tempDir, "account_templates"));
+      fs.mkdirSync(path.join(tempDir, "account_templates", "name_nl"));
+      fs.writeFileSync(configPath, JSON.stringify(existingConfig));
+
+      // Check existing config file before save
+      expect(fs.existsSync(configPath)).toBe(true);
+      let configSaved = JSON.parse(await fsPromises.readFile(configPath, "utf-8"));
+      expect(configSaved).toEqual(existingConfig);
+
+      await AccountTemplate.save("firm", 100, template);
+
+      // Check config file after save
+      configToWrite.id[200] = 505050;
+      expect(fs.existsSync(configPath)).toBe(true);
+      configSaved = JSON.parse(await fsPromises.readFile(configPath, "utf-8"));
+      expect(configSaved).toEqual(configToWrite);
+    });
+
+    it("should replace existing liquid files if the template already exists", async () => {
+      templateUtils.missingLiquidCode.mockReturnValue(false);
+      templateUtils.checkValidName.mockReturnValue(true);
+      templateUtils.filterParts.mockReturnValue(textParts);
+
+      fs.mkdirSync(path.join(tempDir, "account_templates"));
+      fs.mkdirSync(path.join(tempDir, "account_templates", "name_nl"));
+      fs.mkdirSync(path.join(tempDir, "account_templates", "name_nl", "text_parts"));
+      fs.writeFileSync(configPath, JSON.stringify(existingConfig));
+      fs.writeFileSync(mainLiquidPath, "Main part: existing content");
+      fs.writeFileSync(part1LiquidPath, "Part 1: existing content");
+
+      await AccountTemplate.save("firm", 100, template);
+
+      // Check main liquid file
+      const mainLiquidContent = await fsPromises.readFile(mainLiquidPath, "utf-8");
+      expect(mainLiquidContent).toBe(template.text);
+      // Check text parts liquid files
+      const part1LiquidContent = await fsPromises.readFile(part1LiquidPath, "utf-8");
+      expect(part1LiquidContent).toBe(textParts.part_1);
+    });
+
+    it("should not replace existing liquid test files if the template already exists", async () => {
+      templateUtils.missingLiquidCode.mockReturnValue(false);
+      templateUtils.checkValidName.mockReturnValue(true);
+      templateUtils.filterParts.mockReturnValue(textParts);
+
+      const existingLiquidTest = "Existing Liquid Test";
+
+      fs.mkdirSync(path.join(tempDir, "account_templates"));
+      fs.mkdirSync(path.join(tempDir, "account_templates", "name_nl"));
+      fs.mkdirSync(path.join(tempDir, "account_templates", "name_nl", "tests"));
+      fs.writeFileSync(configPath, JSON.stringify(existingConfig));
+      fs.writeFileSync(testLiquidPath, existingLiquidTest);
+
+      await AccountTemplate.save("firm", 100, template);
+
+      // Check liquid test file
+      const testLiquidContent = await fsPromises.readFile(testLiquidPath, "utf-8");
+      expect(testLiquidContent).toBe(existingLiquidTest);
+    });
+
+    // NOTE: Do we need to modify this behavior?
+    it("should not replace or delete unspecified text_parts", async () => {
+      templateUtils.missingLiquidCode.mockReturnValue(false);
+      templateUtils.checkValidName.mockReturnValue(true);
+      templateUtils.filterParts.mockReturnValue(textParts);
+
+      const existingPartContent = "Old part: existing Part Content";
+
+      fs.mkdirSync(path.join(tempDir, "account_templates"));
+      fs.mkdirSync(path.join(tempDir, "account_templates", "name_nl"));
+      fs.mkdirSync(path.join(tempDir, "account_templates", "name_nl", "text_parts"));
+      fs.writeFileSync(configPath, JSON.stringify(existingConfig));
+      fs.writeFileSync(oldPartLiquidPath, existingPartContent);
+
+      await AccountTemplate.save("firm", 100, template);
+
+      // Check Old Part liquid file
+      const oldPartLiquidContent = await fsPromises.readFile(oldPartLiquidPath, "utf-8");
+      expect(oldPartLiquidContent).toBe(existingPartContent);
+    });
   });
-
-  it("should fetch an existing template's config and update with new details", async () => {
-    templateUtils.missingLiquidCode.mockReturnValue(false);
-    templateUtils.checkValidName.mockReturnValue(true);
-    templateUtils.filterParts.mockReturnValue(textParts);
-
-    fs.mkdirSync(path.join(tempDir, "account_templates"));
-    fs.mkdirSync(path.join(tempDir, "account_templates", "name_nl"));
-    fs.writeFileSync(configPath, JSON.stringify(existingConfig));
-
-    // Check existing config file before save
-    expect(fs.existsSync(configPath)).toBe(true);
-    let configSaved = JSON.parse(await fsPromises.readFile(configPath, "utf-8"));
-    expect(configSaved).toEqual(existingConfig);
-
-    await AccountTemplate.save("firm", 100, template);
-
-    // Check config file after save
-    configToWrite.id[200] = 505050;
-    expect(fs.existsSync(configPath)).toBe(true);
-    configSaved = JSON.parse(await fsPromises.readFile(configPath, "utf-8"));
-    expect(configSaved).toEqual(configToWrite);
-
-  });
-
-  it("should replace existing liquid files if the template already exists", async () => {
-    templateUtils.missingLiquidCode.mockReturnValue(false);
-    templateUtils.checkValidName.mockReturnValue(true);
-    templateUtils.filterParts.mockReturnValue(textParts);
-
-    fs.mkdirSync(path.join(tempDir, "account_templates"));
-    fs.mkdirSync(path.join(tempDir, "account_templates", "name_nl"));
-    fs.mkdirSync(path.join(tempDir, "account_templates", "name_nl", "text_parts"));
-    fs.writeFileSync(configPath, JSON.stringify(existingConfig));
-    fs.writeFileSync(mainLiquidPath, "Main part: existing content");
-    fs.writeFileSync(part1LiquidPath, "Part 1: existing content");
-
-    await AccountTemplate.save("firm", 100, template);
-
-    // Check main liquid file
-    const mainLiquidContent = await fsPromises.readFile(mainLiquidPath, "utf-8");
-    expect(mainLiquidContent).toBe(template.text);
-    // Check text parts liquid files
-    const part1LiquidContent = await fsPromises.readFile(part1LiquidPath, "utf-8");
-    expect(part1LiquidContent).toBe(textParts.part_1);
-  });
-
-  it("should not replace existing liquid test files if the template already exists", async () => {
-    templateUtils.missingLiquidCode.mockReturnValue(false);
-    templateUtils.checkValidName.mockReturnValue(true);
-    templateUtils.filterParts.mockReturnValue(textParts);
-
-    const existingLiquidTest = "Existing Liquid Test";
-
-    fs.mkdirSync(path.join(tempDir, "account_templates"));
-    fs.mkdirSync(path.join(tempDir, "account_templates", "name_nl"));
-    fs.mkdirSync(path.join(tempDir, "account_templates", "name_nl", "tests"));
-    fs.writeFileSync(configPath, JSON.stringify(existingConfig));
-    fs.writeFileSync(testLiquidPath, existingLiquidTest);
-
-    await AccountTemplate.save("firm", 100, template);
-
-    // Check liquid test file
-    const testLiquidContent = await fsPromises.readFile(testLiquidPath, "utf-8");
-    expect(testLiquidContent).toBe(existingLiquidTest);
-  });
-
-  // NOTE: Do we need to modify this behavior?
-  it("should not replace or delete unspecified text_parts", async () => {
-    templateUtils.missingLiquidCode.mockReturnValue(false);
-    templateUtils.checkValidName.mockReturnValue(true);
-    templateUtils.filterParts.mockReturnValue(textParts);
-
-    const existingPartContent = "Old part: existing Part Content";
-
-    fs.mkdirSync(path.join(tempDir, "account_templates"));
-    fs.mkdirSync(path.join(tempDir, "account_templates", "name_nl"));
-    fs.mkdirSync(path.join(tempDir, "account_templates", "name_nl", "text_parts"));
-    fs.writeFileSync(configPath, JSON.stringify(existingConfig));
-    fs.writeFileSync(oldPartLiquidPath, existingPartContent);
-
-    await AccountTemplate.save("firm", 100, template);
-
-    // Check Old Part liquid file
-    const oldPartLiquidContent = await fsPromises.readFile(oldPartLiquidPath, "utf-8");
-    expect(oldPartLiquidContent).toBe(existingPartContent);
-  });
-
-});
-
 });
